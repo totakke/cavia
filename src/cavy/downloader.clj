977cd1f203d83ab824710ff150b783f35bd89dbc
Toshiki TAKEUCHI <take@xcoo.jp>
2014-01-17 21:47:07 +0900

    Clear warn on reflection

--- src/cavy/downloader.clj
+++ src/cavy/downloader.clj
@@ -1,7 +1,8 @@
 (ns cavy.downloader
   (:require [clojure.string :as string]
             [clj-http.client :as client]
-            [cavy.common :refer :all]))
+            [cavy.common :refer :all])
+  (:import java.io.FilterInputStream))
 
 (defn- print-progress
   [now total]
@@ -19,9 +20,10 @@
                       (if-let [{:keys [type user password]} auth]
                         {(keyword (str (name type) "-auth")) [user password]}))
         response (client/get url option)
-        content-len (Integer. (get-in response [:headers "content-length"]))
-        is (:body response)
+        content-len (Integer. ^String (get-in response [:headers "content-length"]))
+        is ^FilterInputStream (:body response)
         data (byte-array 1024)]
+    (println (type is))
     (with-open [w (clojure.java.io/output-stream f)]
       (loop [len (.read is data)
              sum len]
