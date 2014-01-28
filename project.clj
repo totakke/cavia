424f693f93731e80fc3d894ebfb2143df4b02b05
Toshiki TAKEUCHI <take@xcoo.jp>
2014-01-28 14:20:03 +0900

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cavy "0.1.1-SNAPSHOT"
   :description "A test resources manager for Clojure"
-  :url "https://github.com/FIXME/cavy"
+  :url "https://github.com/totakke/cavy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -8,4 +8,5 @@
                  [pandect "0.3.0"]
                  [clj-http "0.7.8"]
                  [com.velisco/clj-ftp "0.3.0"]
-                 [clojurewerkz/urly "1.0.0"]])
+                 [clojurewerkz/urly "1.0.0"]]
+  :signing {:gpg-key "roimisia@gmail.com"})
