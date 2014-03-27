fd4d45da57fbb4118889002b06cb8ebc38c5f8f8
Toshiki TAKEUCHI <take@xcoo.jp>
2014-03-27 19:05:41 +0900

    Bump up versions of pandect and clj-http

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [me.raynes/fs "1.4.5"]
-                 [pandect "0.3.0"]
-                 [clj-http "0.7.8"]
+                 [pandect "0.3.1"]
+                 [clj-http "0.9.1"]
                  [com.velisco/clj-ftp "0.3.0"]
                  [clojurewerkz/urly "1.0.0"]]
   :signing {:gpg-key "roimisia@gmail.com"})
