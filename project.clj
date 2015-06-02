5825207d9429e484dbf541bb56c8a71487a70f02
Toshiki Takeuchi <roimisia@gmail.com>
2015-06-02 20:34:35 +0900

    Bump dependencies version up

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [me.raynes/fs "1.4.6"]
-                 [pandect "0.5.1"]
-                 [clj-http "1.0.1"]
+                 [pandect "0.5.2"]
+                 [clj-http "1.1.2"]
                  [com.velisco/clj-ftp "0.3.3"]
                  [clojurewerkz/urly "1.0.0"]]
   :signing {:gpg-key "roimisia@gmail.com"})
