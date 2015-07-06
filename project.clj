50bea88d28975080ed67d16ed04f8a2972ec76c6
Toshiki Takeuchi <roimisia@gmail.com>
2015-07-06 18:03:18 +0900

    Bump up clojure to v1.7

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "https://totakke.github.io/cavia/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.raynes/fs "1.4.6"]
                  [pandect "0.5.2"]
                  [clj-http "1.1.2"]
                  [com.velisco/clj-ftp "0.3.3"]
                  [clojurewerkz/urly "1.0.0"]]
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :signing {:gpg-key "roimisia@gmail.com"})
