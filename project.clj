f0ca57cc03b4d80acb29e1ac15693176144493d3
Toshiki Takeuchi <roimisia@gmail.com>
2015-08-23 21:40:24 +0900

    Use clj-http-lite instead of clj-http

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.raynes/fs "1.4.6"]
                  [pandect "0.5.2"]
-                 [clj-http "1.1.2"]
+                 [clj-http-lite "0.3.0"]
                  [com.velisco/clj-ftp "0.3.3"]
                  [com.cemerick/url "0.1.1"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
