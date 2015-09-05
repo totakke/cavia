79edbe7e0fcfc01bc7b824a6e7a248eb4a5b1539
Toshiki Takeuchi <roimisia@gmail.com>
2015-09-05 11:08:09 +0900

    Release version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cavia "0.1.6-SNAPSHOT"
+(defproject cavia "0.2.0"
   :description "A test resource manager for Clojure"
   :url "https://totakke.github.io/cavia/"
   :license {:name "Eclipse Public License"
