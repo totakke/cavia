7e33cbcb6ba13c563e988165d75ed552624d7596
Toshiki Takeuchi <roimisia@gmail.com>
2016-09-01 16:33:22 +0900

    Add a test profile for Clojure 1.9

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "https://totakke.github.io/cavia/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [me.raynes/fs "1.4.6"]
                  [clj-http-lite "0.3.0"]
                  [com.cemerick/url "0.1.1"]
                  [commons-net "3.5"]
                  [digest "1.4.4"]
                  [progrock "0.1.1"]]
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha11"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
   :signing {:gpg-key "roimisia@gmail.com"})
