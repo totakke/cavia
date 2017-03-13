933e074b28daa496935e97a2b9718a21bdb583cc
Toshiki Takeuchi <roimisia@gmail.com>
2017-03-13 11:22:12 +0900

    Modify clojure version for development

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://totakke.github.io/cavia/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [me.raynes/fs "1.4.6"]
                  [clj-http-lite "0.3.0"]
                  [com.cemerick/url "0.1.1"]
@@ -12,5 +12,5 @@
                  [progrock "0.1.2"]]
   :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :signing {:gpg-key "roimisia@gmail.com"})
