ad5f047acecf8b37f8b78fc99402a8b0f2a503a9
Toshiki Takeuchi <roimisia@gmail.com>
2021-05-14 19:34:53 +0900

    Drop support for clojure 1.7

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/totakke/cavia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.8.0"]
                  [com.jcraft/jsch "0.1.54"]
                  [commons-net "3.6"]
@@ -13,7 +13,6 @@
                  [progrock "0.1.2"]]
   :profiles {:dev {:dependencies [[org.mockftpserver/MockFtpServer "2.7.1"]]}
              :test {:dependencies [[org.slf4j/slf4j-nop "1.7.25"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-alpha5"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :signing {:gpg-key "roimisia@gmail.com"})
