eaf6b521830d3eef69b5195e4b5b4d63a6cfe346
Toshiki Takeuchi <roimisia@gmail.com>
2021-05-18 13:21:17 +0900

    Update deps and drop clojure 1.8

--- project.clj
+++ project.clj
@@ -3,16 +3,15 @@
   :url "https://github.com/totakke/cavia"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.8.0"]
-                 [com.jcraft/jsch "0.1.54"]
-                 [commons-net "3.6"]
-                 [digest "1.4.6"]
-                 [lambdaisland/uri "1.1.0"]
-                 [org.apache.commons/commons-compress "1.16.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-http "3.12.1"]
+                 [com.jcraft/jsch "0.1.55"]
+                 [commons-net "3.8.0"]
+                 [digest "1.4.10"]
+                 [lambdaisland/uri "1.4.54"]
+                 [org.apache.commons/commons-compress "1.20"]
                  [progrock "0.1.2"]]
-  :profiles {:dev {:dependencies [[org.mockftpserver/MockFtpServer "2.7.1"]]}
-             :test {:dependencies [[org.slf4j/slf4j-nop "1.7.25"]]}
-             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :profiles {:dev {:dependencies [[org.mockftpserver/MockFtpServer "2.8.0"]]}
+             :test {:dependencies [[org.slf4j/slf4j-nop "1.7.30"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}}
   :signing {:gpg-key "roimisia@gmail.com"})
