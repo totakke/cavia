6d44017537ad451a37d5cc2948c5b34faf44c5a9
Toshiki Takeuchi <roimisia@gmail.com>
2022-03-24 16:48:18 +0900

    Prepare for next development iteration (0.6.1-SNAPSHOT)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cavia "0.6.0"
+(defproject cavia "0.6.1-SNAPSHOT"
   :description "Test resource manager for Clojure project"
   :url "https://github.com/totakke/cavia"
   :license {:name "Eclipse Public License"
